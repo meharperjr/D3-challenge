fbefd169302fccf0148c585e7f6b9d86762b14f1
unknown <meharperjr@gmail.com>
2019-09-30 22:46:32 -0700

    update

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,102 @@
-// @TODO: YOUR CODE HERE!
+// Set up chart
+var svgWidth = 960;
+var svgHeight = 500;
+var margin = {top: 20, right: 40, bottom: 60, left: 100};
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3
+  .select('.chart')
+  .append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+var chart = svg.append('g');
+
+// Append a div to the body to create tooltips, assign it a class
+d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
+
+
+
+d3.csv("assets/data/data.csv").then(function(myData) {
+    
+ 
+    // number conversion
+    myData.forEach(function(xdata) {
+        xdata.poverty = +xdata.poverty;
+        xdata.healthcare = +xdata.healthcare;
+
+    });
+
+    // x function
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(myData, d=>d.poverty)*0.9,
+            d3.max(myData, d => d.poverty)*1.1])
+        .range([0, width]);
+
+    // y function
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(myData, d => d.healthcare)*1.1])
+        .range([height, 0]);
+
+    // set bottom/left axes
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // x axis
+    chart.append("svg")
+        .attr("transform", `translate(0, ${height})`)
+        .style("font-size", "16px")
+        .call(bottomAxis);
+
+    // y axis
+    chart.append("svg")
+        .style("font-size", "16px")
+        .call(leftAxis);
+
+    // function for circles
+    chart.selectAll("circle")
+        .data(myData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", 12)
+        .attr("fill", "blue")
+        .attr("opacity", ".6");
+
+    // add State abbrev to circles
+    chart.selectAll("text.text-circles")
+        .data(myData)
+        .enter()
+        .append("text")
+        .classed("text-circles",true)
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d.poverty))
+        .attr("y", d => yLinearScale(d.healthcare))
+        .attr("dy",5)
+        .attr("text-anchor","middle")
+        .attr("font-size","12px")
+        .attr("fill", "white");
+
+    // y axis
+    chart.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 30 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .classed("aText", true)
+        .text("Lacks Healthcare (%)");
+
+    // x axis
+    chart.append("text")
+        .attr("y", height + margin.bottom/2 - 10)
+        .attr("x", width / 2)
+        .attr("dy", "1em")
+        .classed("aText", true)
+        .text("Poverty Rate (%)");
+
+
+});
